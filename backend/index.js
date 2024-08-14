82b901b1284702aa1c69e238798d05f2d2fdde38
Ankit Kumar Jha <132083151+Ankitkrjha32@users.noreply.github.com>
2024-08-14 20:37:11 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -18,13 +18,13 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
-const corsOptions = {
-  origin: 'https://frontend-eight-roan-64.vercel.app',
-  methods:["GET" , "POST", "PUT" , "DELETE"],
-  credentials: true
-};
+// const corsOptions = {
+//   origin: 'https://frontend-eight-roan-64.vercel.app',
+//   methods:["GET" , "POST", "PUT" , "DELETE"],
+//   credentials: true
+// };
 
-app.use(cors(corsOptions));
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
