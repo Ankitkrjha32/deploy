9b33e1009849f7a107310260ce01730ccb143b5c
Ankit Kumar Jha <132083151+Ankitkrjha32@users.noreply.github.com>
2024-08-14 16:43:23 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -19,7 +19,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
 const corsOptions = {
-  origin: 'http://localhost:3000',
+  origin: 'https://deploy-9cea.onrender.com',
   credentials: true
 };
 
@@ -28,7 +28,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 
-const PORT = process.env.PORT || 8000;
+const PORT = process.env.PORT || 3000;
 
 // APIs
 app.use('/api/v1/user', userRoute);
