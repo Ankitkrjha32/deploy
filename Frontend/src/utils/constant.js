4f9d53ae934279c82d392548d1d14572da4d80f5
Ankit Kumar Jha <132083151+Ankitkrjha32@users.noreply.github.com>
2024-08-15 22:01:27 +0530

    Update constant.js

--- Frontend/src/utils/constant.js
+++ Frontend/src/utils/constant.js
@@ -1,1 +1,3 @@
-export const USER_API_END_POINT="https://deploy-dun-nine.vercel.app/api/v1/user";
+export const USER_API_END_POINT = process.env.NODE_ENV === 'production' 
+  ? 'https://deploy-dun-nine.vercel.app/api/v1/user' 
+  : 'http://localhost:8000/api/v1/user';
