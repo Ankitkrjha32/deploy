67255943b45a6eafa7096ecb960052de3c0bbdef
Ankit Kumar Jha <132083151+Ankitkrjha32@users.noreply.github.com>
2024-08-22 09:30:31 +0530

    Update index.js

--- Frontend/src/index.js
+++ Frontend/src/index.js
@@ -3,9 +3,9 @@ import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App.js';
 import { Provider } from 'react-redux';
-import store from './redux/store.js';
+import store from './redux/store.jsx';
 import { Toaster } from 'sonner';
-import { UserProvider } from './components/Context/UserContext.js'; // Adjust the path as necessary
+import { UserProvider } from './components/Context/UserContext.jsx'; // Adjust the path as necessary
 
 // Get the root element
 const container = document.getElementById('root');
