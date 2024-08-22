6f480194def5c42f0c32c7a4e9da012316e26212
Ankit Kumar Jha <132083151+Ankitkrjha32@users.noreply.github.com>
2024-08-22 09:17:15 +0530

    Update store.jsx

--- Frontend/src/redux/store.jsx
+++ Frontend/src/redux/store.jsx
@@ -1,6 +1,6 @@
 // src/redux/store.js
 import { configureStore } from '@reduxjs/toolkit';
-import authReducer from './authSlice.js';
+import authReducer from './authSlice';
 
 const store = configureStore({
   reducer: {
