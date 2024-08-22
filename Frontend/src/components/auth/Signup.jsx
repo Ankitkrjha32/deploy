241c195a8741d7c36f29527d5288e53445009254
Ankit Kumar Jha <132083151+Ankitkrjha32@users.noreply.github.com>
2024-08-22 09:33:40 +0530

    Update Signup.jsx

--- Frontend/src/components/auth/Signup.jsx
+++ Frontend/src/components/auth/Signup.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import Navbar from '../shared/Navbar.js';
+import Navbar from '../shared/Navbar.jsx';
 import { Label } from '../ui/label';
 import { Input } from '../ui/input';
 import { RadioGroup } from '../ui/radio-group';
@@ -8,7 +8,7 @@ import { Link, useNavigate } from 'react-router-dom';
 import axios from 'axios';
 import { toast } from 'sonner';
 import { useDispatch, useSelector } from 'react-redux';
-import { setLoading, setUser } from '../../redux/authSlice.js';
+import { setLoading, setUser } from '../../redux/authSlice.jsx';
 import { Loader2 } from 'lucide-react';
 // import { USER_API_END_POINT } from '../../utils/constant.js';
 // import { useNavigate } from 'react-router-dom';
